812b2dca584eaa71f7eb4ace6d7a6c615e661318
Leonardo Lamoia Rosa <leonardotcinf@gmail.com>
2023-11-03 16:24:22 -0300

    Division into Sessions

--- KingBurguer/Feed/View/FeedViewController.swift
+++ KingBurguer/Feed/View/FeedViewController.swift
@@ -14,7 +14,7 @@ class FeedViewController: UIViewController {
     // 3 . definir métodos obrigatórios ( numberOfRowsInSection / cellForRowAt )
     
     private let homeFeedTable: UITableView = {
-        let tv = UITableView()
+        let tv = UITableView(frame: .zero, style: .grouped)
         
         tv.register(FeedTableViewCell.self, forCellReuseIdentifier: FeedTableViewCell.identifier)
         tv.backgroundColor = UIColor.cyan
@@ -29,6 +29,7 @@ class FeedViewController: UIViewController {
         
         view.addSubview(homeFeedTable)
         
+        homeFeedTable.delegate = self
         homeFeedTable.dataSource = self
     }
     
@@ -40,15 +41,24 @@ class FeedViewController: UIViewController {
 }
 
 
-extension FeedViewController: UITableViewDataSource {
+extension FeedViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 20
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 30
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 200
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: FeedTableViewCell.identifier, for: indexPath) as! FeedTableViewCell
         
-        cell.textLabel?.text = "Opa bebê \(indexPath.row)"
+        cell.textLabel?.text = "Opa bebê \(indexPath.section) \(indexPath.row)"
         
         return cell
     }
