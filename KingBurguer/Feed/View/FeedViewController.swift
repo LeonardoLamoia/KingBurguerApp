50287fd145ae499e7d063fcb6b9f820a024b866c
Leonardo Lamoia Rosa <leonardotcinf@gmail.com>
2023-11-03 15:05:52 -0300

    Data Collections with TableView

--- KingBurguer/Feed/View/FeedViewController.swift
+++ KingBurguer/Feed/View/FeedViewController.swift
@@ -9,16 +9,49 @@ import UIKit
 
 class FeedViewController: UIViewController {
     
-    let test: UIView = {
-        let v = UIView(frame: CGRect(x: 20, y: 90, width: 20, height: 20))
-        v.backgroundColor = .purple
-        return v
+    // 1 . registrar uma calsse que seja UITableViewcell
+    // 2 . definir o dataSource (ViewController - que administra fonte de dados)
+    // 3 . definir métodos obrigatórios ( numberOfRowsInSection / cellForRowAt )
+    
+    private let homeFeedTable: UITableView = {
+        let tv = UITableView()
+        
+        tv.register(UITableViewCell.self, forCellReuseIdentifier: "cellId")
+        tv.backgroundColor = UIColor.cyan
+        
+        return tv
     }()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.addSubview(test)
-       
+        view.backgroundColor = .systemBackground
+        
+        view.addSubview(homeFeedTable)
+        
+        homeFeedTable.dataSource = self
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        homeFeedTable.frame = view.bounds
     }
 
+}
+
+
+extension FeedViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 30
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath)
+        
+        cell.textLabel?.text = "Opa bebê \(indexPath.row)"
+        
+        return cell
+    }
+    
+    
 }
